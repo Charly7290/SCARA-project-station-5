9864816b51fdd57df1b363963fde470c489df0cd
Carlos Daniel Murillo Mena <carlosdanielmurillomena@gmail.com>
2026-08-31 00:41:58 -0500

    Funciones de jog para el servo

--- prismatic.h
+++ prismatic.h
@@ -8,3 +8,5 @@ void init_servo(void);
 void init_tool(void);
 int map(int x, int in_min, int in_max, int out_min, int out_max); // Function to map a value from one range to another
 void servo_home(void);
+void servo_jog_up(void);   // Manual jog: increase servo angle by SERVO_JOG_STEP_DEG 
+void servo_jog_down(void); // Manual jog: decrease servo angle by SERVO_JOG_STEP_DEG 
